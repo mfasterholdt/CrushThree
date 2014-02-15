9cda79805393858a96477ec86017b534f4ce05c0
Martin Fasterholdt <mfasterholdt@gmail.com>
2014-02-15 22:42:06 +0100

    Working on scale tool

--- Assets/Editor/ScaleTool.cs
+++ Assets/Editor/ScaleTool.cs
@@ -4,19 +4,14 @@ using UnityEngine;
 [InitializeOnLoad]
 public class ArrowCap : Editor 
 {
-	float arrowSize = 1;
-	
-	void OnSceneGUI () 
+	static ArrowCap()
 	{
-		Debug.Log("hey");
-		//Handles.color = Color.red;
+		SceneView.onSceneGUIDelegate +=DrawArrows;
+	}
 
-		//Handles.ArrowCap(0, target.transform.position +  new Vector3(5,0,0), target.transform.rotation, arrowSize);
-
-
-		//Handles.color = Color.green;
-
-		//Handles.ArrowCap(0, target.transform.position + new Vector3(0,5,0), target.transform.rotation, arrowSize);    	
+	static void DrawArrows (SceneView view)
+	{
+		/*float arrowSize = 2f;
 
 		Transform obj = Selection.activeTransform;
 
@@ -24,7 +19,15 @@ public class ArrowCap : Editor
 			return;
 
 		Handles.color = Color.blue;
-		Handles.ArrowCap(0, Vector3.right * 5f, obj.transform.rotation, arrowSize);
-		//Handles.ArrowCap(0, target.transform.position + new  Vector3(0,0,5), target.transform.rotation, arrowSize);
+		Handles.ArrowCap(0, Vector3.right * 5f, obj.rotation, arrowSize);
+
+		Vector3 currentScale = obj.localScale;
+
+		Handles.ArrowCap(0, obj.position + Vector3.right, obj.rotation, arrowSize);
+
+		if(GUI.changed)
+			EditorUtility.SetDirty (obj);*/
 	}
 }
+
+//Handles.ArrowCap(0, target.transform.position + new  Vector3(0,0,5), target.transform.rotation, arrowSize);
